2b769b6ba2f8d2618babf110e5236ecb80f94419
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-19 19:59:55 -0600

    Day 2: System clock created

--- oss.h
+++ oss.h
@@ -4,7 +4,7 @@
 struct PCB {
 int occupied; // either true or false
 pid_t pid; // process id of this child
-int sec; // time when it was forked
-int nano; // time when it was forked
+double sec; // time when it was forked
+double nano; // time when it was forked
 };
 struct PCB processTable[20];
