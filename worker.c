ce1ff2b2c19e4c184bbf203a6957ea9798c92b74
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-19 19:55:49 -0600

    README

--- worker.c
+++ worker.c
@@ -8,19 +8,22 @@ int main(int argc, char *argv[]){
     printf("Child got sh_key: %i", sh_key);
 
 
-    int shm_id = shmget(sh_key, sizeof(int)*10, 0666);
+    int shm_id = shmget(sh_key, sizeof(struct PCB), 0666);
     if(shm_id <= 0) {
         fprintf(stderr,"CHILD ERROR: Failed to get shared memory, shared memory id = %i\n", shm_id);
         exit(1);
     }
 
     //attatch memory we allocated to our process and point pointer to it
-    int *shm_ptr = (int*) (shmat(shm_id, 0, 0));
+    struct PCB *shm_ptr = (int*) (shmat(shm_id, 0, 0));
     if (shm_ptr <= 0) {
         fprintf(stderr,"Child Shared memory attach failed\n");
         exit(1);
     }
-    printf("Child: Read Value: %d\n", *shm_ptr);
+    struct PCB readFromMem;
+    readFromMem = *shm_ptr;
+    
+    printf("Child: Read Value - memSec: %lf memNano: %lf \n", readFromMem.sec, readFromMem.nano);
 
     return 0;
 //     WORKER PID:6577 PPID:6576 SysClockS: 5 SysclockNano: 1000 TermTimeS: 11 TermTimeNano: 500100
