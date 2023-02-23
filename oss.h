f2befa3738d91dcf09c2adc8ac933b901f55bee7
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-22 18:55:29 -0600

    Memory

--- oss.h
+++ oss.h
@@ -3,9 +3,9 @@
 
 
 struct PCB {
-// int occupied; // either true or false
-// pid_t pid; // process id of this child
+int occupied; // either true or false
+pid_t pid; // process id of this child
 double sec; // time when it was forked
 double nano; // time when it was forked
 };
-//struct PCB processTable[20];
+struct PCB processTable[20];
