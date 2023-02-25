a89535d6dedb47b13184e4e8af720f1c1e601b74
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-25 13:37:56 -0600

    Memory

--- oss.c
+++ oss.c
@@ -220,6 +220,11 @@ int main(int argc, char *argv[]){
         printf("memSec: %lf memNano: %lf \n", processTable[i].sec, processTable[i].nano);
         *shm_ptr = *processTable;
 
+        processTable[i].nano = 5;
+        processTable[i].sec = 77;
+
+        printf("Fucked up memory memSec: %lf memNano: %lf \n", processTable[i].sec, processTable[i].nano);
+
         *processTable = *shm_ptr;
         printf("Wrote to memory:: memSec: %lf memNano: %lf \n", processTable[i].sec, processTable[i].nano);
 
