367cd3063d70ece837507cd5f40cdcaeb7ea2ee9
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-26 18:29:38 -0600

    getting children to loop and terminate

--- oss.c
+++ oss.c
@@ -278,6 +278,8 @@ void printTable(){
     printf("Entry\tOccupied\tPID\t\tStartS\t\tStartN\n");
     int i;
     for(i=0;i<20;i++){
-        printf("%i\t%d\t\t%ld\t\t%f\t\t%f\n", i, processTable[i].occupied, (long)processTable[i].pid, processTable[i].sec, processTable[i].nano);
+        if(processTable[i].pid != 0 ){
+            printf("%i\t%d\t\t%ld\t\t%f\t\t%f\n", i, processTable[i].occupied, (long)processTable[i].pid, processTable[i].sec, processTable[i].nano);
+        }
     }
 }
