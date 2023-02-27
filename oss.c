43d3928b458a1d7f0f21579d41dd717cd66ede1d
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-26 18:30:17 -0600

    getting children to loop and terminate

--- oss.c
+++ oss.c
@@ -202,7 +202,7 @@ int main(int argc, char *argv[]){
         }
 
         if((childrenToLaunch >= proc) && (allChildrenHaveFinished)){    //Check if all children have been created, check if all children have finished
-                printf("OSS PID: %ld SysClockS: %i SysclockNano: %i\n", (long)getpid(), sec, nano);
+                printf("OSS PID: %ld SysClockS: %f SysclockNano: %f\n", (long)getpid(), sec, nano);
                 printf("Process Table:\n");
                 printTable();
             break; //program can end, all child processes are done
